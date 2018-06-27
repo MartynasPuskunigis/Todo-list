a40e4ab1982c2ab00ff3ed898107d882c8d4b31f
MartynasP <39139245+RealFisherman@users.noreply.github.com>
2018-06-27 12:50:18 +0300

    init commit

---
+++ src/app/User.ts
@@ -0,0 +1,8 @@
+export interface User {
+    id?: number;
+    first_name?: string | null;
+    last_name?: string | null;
+    email?: string | null;
+    gender?: string | null;
+    ip_address?: string | null;
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,96 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { User } from "./User";
+import { data } from "./data";
 
-class App extends React.Component {
-    public render(): JSX.Element {
-        return <div>Index</div>;
-    }
+interface Props {}
+
+interface State {
+  baseuserdata: User[];
+  userdata: User[];
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      baseuserdata: [],
+      userdata: []
+    };
+  }
+
+  public componentDidMount() {
+    this.setState({
+      userdata: data,
+      baseuserdata: data
+    });
+  }
+
+  protected setMaleFilter: React.MouseEventHandler<HTMLButtonElement> = () => {
+    this.setState({
+      userdata: this.state.baseuserdata.filter(x => x.gender === "Male")
+    });
+  };
+
+  protected setFemaleFilter: React.MouseEventHandler<
+    HTMLButtonElement
+  > = () => {
+    this.setState({
+      userdata: this.state.baseuserdata.filter(x => x.gender === "Female")
+    });
+  };
+  protected resetFilter: React.MouseEventHandler<HTMLButtonElement> = () => {
+    this.setState({
+      userdata: this.state.baseuserdata
+    });
+  };
+
+  protected setNameFilter: React.ChangeEventHandler<
+    HTMLInputElement
+  > = event => {
+    this.setState({
+      userdata: this.state.baseuserdata.filter(x =>
+        x.first_name !== null && undefined ? x.first_name.startsWith(event.target.value) : ""
+      )
+    });
+  };
+
+  public render(): JSX.Element {
+    return (
+      <div>
+        <button onClick={this.setMaleFilter}>Male</button>
+        <button onClick={this.setFemaleFilter}>Female</button>
+        <button onClick={this.resetFilter}>Reset</button>
+        <input onChange={this.setNameFilter} type="text" />
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>IP Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.userdata.map((data, i) => {
+              return (
+                <tr key={i}>
+                  <td>{data.id}</td>
+                  <td>{data.first_name}</td>
+                  <td>{data.last_name}</td>
+                  <td>{data.email}</td>
+                  <td>{data.gender}</td>
+                  <td>{data.ip_address}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("react-app"));
